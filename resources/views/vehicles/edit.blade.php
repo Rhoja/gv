598265a0cf9785fd7bab420b32dc9d92895307fc
Edith <isc.arizpe@gmail.com>
2019-04-11 16:09:45 -0600

    hola

--- resources/views/vehicles/edit.blade.php
+++ resources/views/vehicles/edit.blade.php
@@ -3,8 +3,4 @@
 
 @section('content')
 {{ $vehicle }}
-<<<<<<< HEAD
-
-=======
->>>>>>> e6dd4d935c19918535be295b6f50910fd232ee72
 @endsection
--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -2,13 +2,9 @@
 @section('title' , 'Vehiculos')
 
 @section('content')
-<<<<<<< HEAD
 
 
 {{ $vehicle }}
 
-=======
-{{ $vehicle }}
->>>>>>> e6dd4d935c19918535be295b6f50910fd232ee72
 
 @endsection
